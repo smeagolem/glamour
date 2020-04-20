942b8615b0f51cd549620c0c109034cccfb81335
David J Holland <stooge213@hotmail.com>
2020-04-20 22:59:21 +1000

    💬 update window name

--- src/sandbox/bin.rs
+++ src/sandbox/bin.rs
@@ -3,7 +3,7 @@ use glamour::Application;
 mod square_layer;
 
 fn main() {
-    let mut app = Application::new("It's a triangle now!", 510, 488);
+    let mut app = Application::new("It's cubes now!", 510, 488);
     let square_layer = square_layer::SquareLayer::new("SquareLayer");
     app.push_layer(Box::new(square_layer));
     app.run();
